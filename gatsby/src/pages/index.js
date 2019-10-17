b3d86dbd5286481c83ce27231e6080debca237bb
unknown <ychen239@uottawa.ca>
2019-10-17 10:08:29 -0400

    new site

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,48 @@
+import withRoot from "./modules/withRoot";
+
+import React from 'react';
+import ProductCategories from './modules/views/ProductCategories';
+import ProductSmokingHero from './modules/views/ProductSmokingHero';
+import AppFooter from './modules/views/AppFooter';
+import ProductHero from './modules/views/ProductHero';
+import ProductValues from './modules/views/ProductValues';
+import ProductHowItWorks from './modules/views/ProductHowItWorks';
+import ProductCTA from './modules/views/ProductCTA';
+import AppAppBar from './modules/views/AppAppBar';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import MuiLink from '@material-ui/core/Link';
+import ProTip from '../components/ProTip';
+import Link from '../components/Link';
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <MuiLink color="inherit" href="https://material-ui.com/">
+        Your Website
+      </MuiLink>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+function Index() {
+  return (
+      <React.Fragment>
+          <AppAppBar />
+          <ProductHero />
+          <ProductValues />
+          <ProductCategories />
+          <ProductHowItWorks />
+          <ProductCTA />
+          <ProductSmokingHero />
+          <AppFooter />
+      </React.Fragment>
+  );
+}
+
+export default withRoot(Index);
